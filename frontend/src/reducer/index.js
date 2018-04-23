903237e1ae572f86832d14bb8d11b3c0c33be816
Maja <manon.jacquin@gmail.com>
2018-04-23 13:53:11 -0700

    Imports actions

---
+++ frontend/src/reducer/index.js
@@ -0,0 +1,14 @@
+import {
+    ADD_POST,
+    SET_FILTER,
+    SHOW_CATEGORIES,
+    // POST_CONTENT,
+    // COMMENT_POST,
+    // COMMENT_USER_POST,
+    // VOTE_POST,
+    // VOTE_COMMENT,
+    // EDIT_COMMENT,
+    // EDIT_POST,
+    // DELETE_COMMENT,
+    // DELETE_POST,
+} from '../actions'
