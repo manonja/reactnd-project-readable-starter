8a44b939c0cda5ee09a1b3b1ffadbc26f810de56
Maja <manon.jacquin@gmail.com>
2018-04-23 13:46:39 -0700

    Adds books.api structure for reference

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,56 @@
+const api = "localhost:3001"
+
+
+
+
+fetch(
+    url,
+    {
+        headers: { 'Authorization': 'whatever-you-want' }
+    }
+)
+
+// Generate a unique token for storing your bookshelf data on the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+// Get all the comments for a single post
+export const getComments = (id) =>
+  fetch(`${api}/posts/${id}/comments`, {
+    method: 'GET',
+    headers
+  }).then(res => res.json())
+
+
+
+export const getAll = () =>
+  fetch(`${api}/books`, { headers })
+    .then(res => res.json())
+    .then(data => data.books)
+
+export const update = (book, shelf) =>
+  fetch(`${api}/books/${book.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ shelf })
+  }).then(res => res.json())
+
+export const search = (query) =>
+  fetch(`${api}/search`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  }).then(res => res.json())
+    .then(data => data.books)
