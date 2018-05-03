bbf5faaa026ef13e921ce3ae2276eb12c0a84d2d
Maja <manon.jacquin@gmail.com>
2018-05-03 12:44:43 -0700

    Install react-redux and wrap App within Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App/> 
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
